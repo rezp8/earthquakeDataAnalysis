325ea91be9d1ee9e54f3f6bbfa922cd264cc8a57
Sepideh Tajrishi <sepideh.tajrishi@gmail.com>
2025-10-14 00:03:28 +0300

    Make minor changes

--- EMSC_web.py
+++ EMSC_web.py
@@ -18,9 +18,9 @@ except:
 
 time.sleep(3)
 start_date = driver.find_element(By.ID, "datemin")
-start_date.send_keys("01/09/2025")
+start_date.send_keys("15/09/2025")
 end_date = driver.find_element(By.ID, "datemax")
-end_date.send_keys("30/09/2025")
+end_date.send_keys("12/10/2025")
 lat_min = driver.find_element(By.ID, "latmin")
 lat_min.send_keys("24")
 lat_max = driver.find_element(By.ID, "latmax")
@@ -48,18 +48,22 @@ try:
             By.CSS_SELECTOR, "table.eqs.table-scroll")
         data_rows = data_table.find_elements(By.TAG_NAME, "tr")[1:]
         for row in data_rows:
-            date_time = row.find_element(By.CLASS_NAME, "tbdat").text.split()
-            date_ = date_time[0]
-            time_ = date_time[1]
+            date_time = row.find_element(By.CLASS_NAME, "tbdat").text
+            # date_time = row.find_element(By.CLASS_NAME, "tbdat").text.split()
+            # date_ = date_time[0]
+            # time_ = date_time[1]
             latitude_deg = row.find_element(By.CLASS_NAME, "tblat").text
             longitude_deg = row.find_element(By.CLASS_NAME, "tblon").text
             depth_km = row.find_element(By.CLASS_NAME, "tbdep").text
             magnitude_type = row.find_element(By.CLASS_NAME, "tbmagtyp").text
             magnitude_value = row.find_element(By.CLASS_NAME, "tbmag").text
             region = row.find_element(By.CLASS_NAME, "tbreg").text
-            earthquake_info = {"date": date_, "time": time_, "latitude_deg": latitude_deg, "longitude_deg": longitude_deg,
+            earthquake_info = {"date_time_UTC": date_time, "latitude_deg": latitude_deg, "longitude_deg": longitude_deg,
                                "depth_km": depth_km, "magnitude_value": magnitude_value, "magnitude_type": magnitude_type,
                                "region": region}
+            # earthquake_info = {"date": date_, "time_UTC": time_, "latitude_deg": latitude_deg, "longitude_deg": longitude_deg,
+            #                    "depth_km": depth_km, "magnitude_value": magnitude_value, "magnitude_type": magnitude_type,
+            #                    "region": region}
             all_rows_info.append(earthquake_info)
         next_page_button = driver.find_element(
             By.CSS_SELECTOR, "div.spes.spes1.pag")
@@ -69,7 +73,7 @@ except:
     print("This was the last page")
 
 print(len(all_rows_info))
-print(all_rows_info[-1])
+# print(all_rows_info[-1])
 
 time.sleep(2)
 earthquakes_table = pd.DataFrame(all_rows_info)
