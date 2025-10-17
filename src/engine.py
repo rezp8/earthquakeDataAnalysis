1eeb9a15a8bd217702d230d520f64f11988d7e52
rezp <reza.pirdeyr8@gamil.com>
2025-10-17 18:10:50 +0330

    update engine.py

--- src/engine.py
+++ src/engine.py
@@ -1,8 +1,38 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
 def get_engine():
     """
     creates a connection (Engine) to the sqlite database.
-    earthquakes.db is created alongside the project.
+    earthquakes.db is created alongside the project.    
     """
-    return create_engine("sqlite:///earthquakes.db", future = True)
+
+    user = "root" # نام کاربری MySQL
+    password = "" # رمز عبور MySQL
+    host = "localhost" # اگر سرور محلی داری
+    port = 3306 # پورت MySQL
+    database = "earthquakeDB" # نام دیتابیس
+
+    # url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4"
+    url = URL.create(
+        drivername="mysql+pymysql",
+        username=user,
+        password=password,   # handles special chars like '@'
+        host=host,
+        port=port,
+        database=database,
+        query={"charset": "utf8mb4"},
+    )
+    return create_engine(url, future=True, pool_pre_ping=True)
+
+
+
+
+# from sqlalchemy import create_engine
+
+# def get_engine():
+#     """
+#     creates a connection (Engine) to the sqlite database.
+#     earthquakes.db is created alongside the project.
+#     """
+#     return create_engine("sqlite:///earthquakes.db", future = True)
