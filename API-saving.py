d82eaa1e88538d143af4f226b307eb88b9605327
Agha-reza <rezaalizadeh84129@gmail.com>
2025-10-18 09:16:15 +0330

    1

---
+++ API-saving.py
@@ -0,0 +1,22 @@
+import requests
+from datetime import datetime, timedelta
+
+# end_date = datetime.today().date()
+# start_date = end_date - timedelta(days=30)
+def api_saving():
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+    params = {
+        "format": "csv",
+        "starttime": "2025-9-15",
+        "endtime": "2025-10-15",
+        "minlatitude": 24,
+        "maxlatitude": 46,
+        "minlongitude": 123,
+        "maxlongitude": 146,
+        "minmagnitude": 0
+    }
+    response = requests.get(url, params=params)
+    with open("japan_earthquakes.csv", "w", encoding="utf-8") as f:
+            f.write(response.text)
+
+api_saving()
